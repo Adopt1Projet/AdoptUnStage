d1584cfe57f675b24c8f0f85276404dc5193df20
Ioannis Nikas <ioa.nikas@gmail.com>
2018-11-28 17:57:13 +0100

    creation navbar/footer/page-accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageAccueilComponent } from './page-accueil/page-accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PageAccueilComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-accueil/page-accueil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-accueil',
+  templateUrl: './page-accueil.component.html',
+  styleUrls: ['./page-accueil.component.css']
+})
+export class PageAccueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
