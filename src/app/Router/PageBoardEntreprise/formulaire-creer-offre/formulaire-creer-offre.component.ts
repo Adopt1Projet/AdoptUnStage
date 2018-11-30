fc88477a6e299b45180f200c09d6f3059a9dff81
Julien Dcode <juliendcode@gmail.com>
2018-11-30 16:47:01 +0100

    formulaire creer annonce entreprise sans sous menu

---
+++ src/app/Router/PageBoardEntreprise/formulaire-creer-offre/formulaire-creer-offre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+// import { Hero } from '../formulaire-creer-offre';
+@Component({
+  selector: 'app-formulaire-creer-offre',
+  templateUrl: './formulaire-creer-offre.component.html',
+  styleUrls: ['./formulaire-creer-offre.component.css']
+})
+export class FormulaireCreerOffreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Router/PageBoardEntreprise/page-board-entreprise/page-board-entreprise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-board-entreprise',
+  templateUrl: './page-board-entreprise.component.html',
+  styleUrls: ['./page-board-entreprise.component.css']
+})
+export class PageBoardEntrepriseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
