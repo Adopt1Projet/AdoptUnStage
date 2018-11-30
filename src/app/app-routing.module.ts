4579fbaee5cc0481050485db4334e1411ef6991e
Ioannis Nikas <ioa.nikas@gmail.com>
2018-11-30 16:38:17 +0100

    Listes offres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageOffresComponent } from './Router/PageOffres/page-offres/page-offres.component';
+import { PageAccueilComponent } from './Router/PageAccueil/page-accueil/page-accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'accueil', component: PageAccueilComponent },
+  { path: 'offres', component: PageOffresComponent },
+  { path : '', redirectTo: 'accueil', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageAccueilComponent } from './Router/PageAccueil/page-accueil/page-accueil.component';
+import { PageOffresComponent } from './Router/PageOffres/page-offres/page-offres.component';
+import { ListeOffresComponent } from './Router/PageOffres//liste-offres/liste-offres.component';
+import { ItemOffreComponent } from './Router/PageOffres//item-offre/item-offre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    PageAccueilComponent
+    PageAccueilComponent,
+    PageOffresComponent,
+    ListeOffresComponent,
+    ItemOffreComponent
   ],
   imports: [
     BrowserModule,
