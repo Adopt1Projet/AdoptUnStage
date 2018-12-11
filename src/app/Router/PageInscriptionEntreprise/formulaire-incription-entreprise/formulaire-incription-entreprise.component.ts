2176cd7d6cff4c0548a1958dcee23787885f5b34
Ioannis Nikas <ioa.nikas@gmail.com>
2018-12-11 14:13:46 +0100

    Ajout des formGroup fonctionnelles pour inscription stagiaire/entreprise pour tests liaison front/back

--- src/app/Router/PageInscriptionEntreprise/formulaire-incription-entreprise/formulaire-incription-entreprise.component.ts
+++ src/app/Router/PageInscriptionEntreprise/formulaire-incription-entreprise/formulaire-incription-entreprise.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-incription-entreprise',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormulaireIncriptionEntrepriseComponent implements OnInit {
 
-  constructor() { }
+  formCreate = new FormGroup({
+    raisonSociale: new FormControl(null, [Validators.required]),
+    secteur: new FormControl(null, [Validators.required]),
+    statutEntreprise: new FormControl(null, [Validators.required]),
+    siteWeb: new FormControl(null),
+    adresse: new FormControl(null, [Validators.required]),
+    ville: new FormControl(null),
+    codePostal: new FormControl(null, [Validators.required]),
+    logo: new FormControl(null),
+    prenom: new FormControl(null, [Validators.required]),
+    nom: new FormControl(null, [Validators.required]),
+    fonction: new FormControl(null),
+    tel: new FormControl(null),
+    mail: new FormControl(null, [Validators.required]),
+    mail2: new FormControl(null, [Validators.required]),
+    mdp: new FormControl(null, [Validators.required]),
+    mdp2: new FormControl(null, [Validators.required])
+    });
+
+  constructor(/*private userService: userService, */ private _location: Location) { }
+
+  retourPage() {
+    this._location.back();
+  }
 
   ngOnInit() {
   }
-
+  /*
+  onSubmit(){
+    const user: User = Object.assign({}, this.formCreate.value);
+    this.userService.createCustomer(user)
+    .subscribe(data => console.log(data), error => console.log(error));
+    this.formCreate.reset();
+  }
+  */
 }
--- src/app/Router/PageInscriptionStagiaire/formulaire-inscription-stagiaire/formulaire-inscription-stagiaire.component.ts
+++ src/app/Router/PageInscriptionStagiaire/formulaire-inscription-stagiaire/formulaire-inscription-stagiaire.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-inscription-stagiaire',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormulaireInscriptionStagiaireComponent implements OnInit {
 
-  constructor() { }
+  formCreate = new FormGroup({
+    prenom: new FormControl(null, [Validators.required]),
+    nom: new FormControl(null, [Validators.required]),
+    etablissement: new FormControl(null, [Validators.required]),
+    ville: new FormControl(null),
+    codePostal: new FormControl(null, [Validators.required]),
+    tel: new FormControl(null),
+    mail: new FormControl(null, [Validators.required]),
+    mail2: new FormControl(null, [Validators.required]),
+    mdp: new FormControl(null, [Validators.required]),
+    mdp2: new FormControl(null, [Validators.required])
+    });
+
+  constructor(/*private userService: userService, */ private _location: Location) { }
 
   ngOnInit() {
   }
 
+  retourPage() {
+    this._location.back();
+  }
+  /*
+  onSubmit(){
+    const user: User = Object.assign({}, this.formCreate.value);
+    this.userService.createCustomer(user)
+    .subscribe(data => console.log(data), error => console.log(error));
+    this.formCreate.reset();
+  }
+  */
 }
