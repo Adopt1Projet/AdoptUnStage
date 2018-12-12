732e1ec492bd6bb855abaddffab92e9497f4f360
Ioannis Nikas <ioa.nikas@gmail.com>
2018-12-12 11:12:45 +0100

    Nouveau Back généré par nos soins !

--- AdoptSPRING/src/test/java/fr/adoptunstage/spring/ApplicationTests.java
+++ AdoptSPRING/src/test/java/fr/adoptunstage/spring/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javasampleapproach.springrest.mysql;
+package fr.adoptunstage.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringRestMySqlApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
