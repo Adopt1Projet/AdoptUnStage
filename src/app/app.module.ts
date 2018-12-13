92f6a8e966534e9b17fcf7bddbea101bb2f8d2b5
Magali Klein <mk.conf69@gmail.com>
2018-12-13 11:11:05 +0100

    réorganisation des import router dans app.module, pour plus de clarté

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,48 +2,67 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Elements fixes sur pages et accueil site: Accueil, Navbar, Footer, boutons accueil et connexion
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageAccueilComponent } from './Router/PageAccueil/page-accueil/page-accueil.component';
+import { BoutonInscriptionAccueilComponent } from './Router/PageAccueil/bouton-inscription-accueil/bouton-inscription-accueil.component';
+// tslint:disable-next-line:max-line-length
+import { BoutonsInscriptionConnexionComponent } from './Router/PageConnexion/boutons-inscription-connexion/boutons-inscription-connexion.component';
+
+// Page Inscription Stagiaire
 // tslint:disable-next-line:max-line-length
 import { PageInscriptionStagiaireComponent } from './Router/PageInscriptionStagiaire/page-inscription-stagiaire/page-inscription-stagiaire.component';
 // tslint:disable-next-line:max-line-length
 import { FormulaireInscriptionStagiaireComponent } from './Router/PageInscriptionStagiaire/formulaire-inscription-stagiaire/formulaire-inscription-stagiaire.component';
 // tslint:disable-next-line:max-line-length
+
+// Page Inscription entreprise
+// tslint:disable-next-line:max-line-length
 import { PageInscriptionEntrepriseComponent } from './Router/PageInscriptionEntreprise/page-inscription-entreprise/page-inscription-entreprise.component';
 // tslint:disable-next-line:max-line-length
 import { FormulaireIncriptionEntrepriseComponent } from './Router/PageInscriptionEntreprise/formulaire-incription-entreprise/formulaire-incription-entreprise.component';
+
+//Page Offres de stages
 import { PageOffresComponent } from './Router/PageOffres/page-offres/page-offres.component';
 import { ListeOffresComponent } from './Router/PageOffres//liste-offres/liste-offres.component';
 import { ItemOffreComponent } from './Router/PageOffres//item-offre/item-offre.component';
-import { BoutonInscriptionAccueilComponent } from './Router/PageAccueil/bouton-inscription-accueil/bouton-inscription-accueil.component';
+
+// Page Postuler
 import { PagePostulerComponent } from './Router/PagePostuler/page-postuler/page-postuler.component';
-import { PageConnexionComponent } from './Router/PageConnexion/page-connexion/page-connexion.component';
-// tslint:disable-next-line:max-line-length
-import { BoutonsInscriptionConnexionComponent } from './Router/PageConnexion/boutons-inscription-connexion/boutons-inscription-connexion.component';
-import { FormulaireConnexionComponent } from './Router/PageConnexion/formulaire-connexion/formulaire-connexion.component';
 import { FormulairePostulerComponent } from './Router/PagePostuler/formulaire-postuler/formulaire-postuler.component';
-import { ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
+
+// Page connexion
+import { PageConnexionComponent } from './Router/PageConnexion/page-connexion/page-connexion.component';
+import { FormulaireConnexionComponent } from './Router/PageConnexion/formulaire-connexion/formulaire-connexion.component';
+
+// Board stagiaire
 import { PageBoardStagiaireComponent } from './Router/BoardStagiaire/page-board-stagiaire/page-board-stagiaire.component';
 import { InfosStagiaireComponent } from './Router/BoardStagiaire/infos-stagiaire/infos-stagiaire.component';
 import { GestionCandidaturesComponent } from './Router/BoardStagiaire/gestion-candidatures/gestion-candidatures.component';
-=======
+
+// Board entreprise
+import { ItemEntrepriseComponent } from './Router/PagePartenaires/item-entreprise/item-entreprise.component';
+import { ListeEntreprisesComponent } from './Router/PagePartenaires/liste-entreprises/liste-entreprises.component';
+
+// Page partenaires
+import { PagePartenairesComponent } from './Router/PagePartenaires/page-partenaires/page-partenaires.component';
 import { ActeursComponent } from './Router/PagePartenaires/acteurs/acteurs.component';
 import { RemerciementsComponent } from './Router/PagePartenaires/remerciements/remerciements.component';
 import { ParticipantsComponent } from './Router/PagePartenaires/participants/participants.component';
-import { ItemEntrepriseComponent } from './Router/PagePartenaires/item-entreprise/item-entreprise.component';
-import { ListeEntreprisesComponent } from './Router/PagePartenaires/liste-entreprises/liste-entreprises.component';
-import { PagePartenairesComponent } from './Router/PagePartenaires/page-partenaires/page-partenaires.component';
+
+// Page détail offres
+import { PageDetailOffreComponent } from './Router/PageDetailOffre/page-detail-offre/page-detail-offre.component';
 import { BoutonsOffreComponent } from './Router/PageDetailOffre/boutons-offre/boutons-offre.component';
 import { DescriptionEntrepriseComponent } from './Router/PageDetailOffre/description-entreprise/description-entreprise.component';
 import { DescriptionOffreComponent } from './Router/PageDetailOffre/description-offre/description-offre.component';
 import { InfosStageComponent } from './Router/PageDetailOffre/infos-stage/infos-stage.component';
-import { PageDetailOffreComponent } from './Router/PageDetailOffre/page-detail-offre/page-detail-offre.component';
->>>>>>> dev
+
+
 
 
 @NgModule({
@@ -65,11 +84,9 @@ import { PageDetailOffreComponent } from './Router/PageDetailOffre/page-detail-o
     BoutonsInscriptionConnexionComponent,
     FormulaireConnexionComponent,
     FormulairePostulerComponent,
-<<<<<<< HEAD
     PageBoardStagiaireComponent,
     InfosStagiaireComponent,
-    GestionCandidaturesComponent
-=======
+    GestionCandidaturesComponent,
     ActeursComponent,
     RemerciementsComponent,
     ParticipantsComponent,
@@ -81,7 +98,6 @@ import { PageDetailOffreComponent } from './Router/PageDetailOffre/page-detail-o
     DescriptionOffreComponent,
     InfosStageComponent,
     PageDetailOffreComponent
->>>>>>> dev
   ],
   imports: [
     BrowserModule,
