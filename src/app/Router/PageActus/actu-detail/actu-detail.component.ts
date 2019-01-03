51f9337008603d9b45b318a81e7722f2c54e1344
Ioannis Nikas <ioa.nikas@gmail.com>
2019-01-03 17:36:43 +0100

    Ajout partie Actualité fonctionnelle papa

--- src/app/Router/PageActus/actu-detail/actu-detail.component.ts
+++ src/app/Router/PageActus/actu-detail/actu-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Actu } from 'src/app/modeles/actu';
+import { Observable } from 'rxjs';
+import { ActuService } from 'src/app/services/actu.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-actu-detail',
@@ -7,11 +11,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ActuDetailComponent implements OnInit {
   
-  @Input() actu: any;
+   actus : Observable<any>;
+   article: Actu;
 
-  constructor() { }
+  constructor(private serviceActu : ActuService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.serviceActu.getActusList()
+      .subscribe(data => {
+        this.actus = data;
+        const id = this.route.snapshot.params['id'];
+        this.article = this.actus[id - 1];
+      });
   }
 
 }
