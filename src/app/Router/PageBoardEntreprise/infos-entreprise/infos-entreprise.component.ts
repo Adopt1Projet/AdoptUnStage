092e3d7fada9fd21344fcdd2c3ae445122a70f3e
Hugo Hontans <hugohontans@gmail.com>
2019-01-04 17:08:48 +0100

    recup de l'entreprise

--- src/app/Router/PageBoardEntreprise/infos-entreprise/infos-entreprise.component.ts
+++ src/app/Router/PageBoardEntreprise/infos-entreprise/infos-entreprise.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EntrepriseService } from '../../../services/entreprise.service';
+import { TokenStorageService } from '../../../auth/token-storage.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-infos-entreprise',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfosEntrepriseComponent implements OnInit {
 
-  constructor() { }
+  username;
+  entreprise : any;
+
+
+
+  constructor(private entrepriseService : EntrepriseService, private token : TokenStorageService) { }
 
   ngOnInit() {
+    this.username = this.token.getUsername();
+    this.entrepriseService.getEntreprise(this.username).subscribe(data =>{ this.entreprise = data;console.log(this.entreprise);}, error => console.log(error));;
   }
 
 }
