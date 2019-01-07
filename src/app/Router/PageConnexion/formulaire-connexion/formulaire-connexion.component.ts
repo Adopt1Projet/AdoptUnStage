da254c34a6f8d3a7f549d2ee22cbdc1b472709e1
Magali Klein <mk.conf69@gmail.com>
2019-01-07 18:04:55 +0100

    modif suite à conflits merge sur formulaire-connexion.component.ts

--- src/app/Router/PageConnexion/formulaire-connexion/formulaire-connexion.component.ts
+++ src/app/Router/PageConnexion/formulaire-connexion/formulaire-connexion.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-<<<<<<< HEAD
-import {
-  FormGroup,
-  FormControl,
-  FormBuilder,
-  Validators
-} from '@angular/forms';
-=======
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
->>>>>>> 7a357d1b24b87b1a503e3066fbe2151f50aee014
 import { AuthService } from '../../../auth/auth.service';
 import { TokenStorageService } from '../../../auth/token-storage.service';
 import { Router } from '@angular/router';
@@ -22,21 +13,6 @@ import { AlertService } from '../../../services/alert.service';
   styleUrls: ['./formulaire-connexion.component.css']
 })
 export class FormulaireConnexionComponent implements OnInit {
-<<<<<<< HEAD
-  public formConnect: FormGroup;
-
-  // formulaireConnexion = new FormGroup({
-  //   username: new FormControl(null, [Validators.required]),
-  //   password: new FormControl(null, [Validators.required])
-  // });
-
-
-  isLoginFailed = false;
-  errorMessage = '';
-  roles: string[] = [];
-  private loginInfo: AuthLoginInfo;
-
-=======
 
   isLoginFailed = false;
   errorMessage = '';
@@ -45,15 +21,11 @@ export class FormulaireConnexionComponent implements OnInit {
   private loginInfo: AuthLoginInfo;
   public formConnect: FormGroup;
 
->>>>>>> 7a357d1b24b87b1a503e3066fbe2151f50aee014
   constructor(
     private authService: AuthService,
     private tokenStorage: TokenStorageService,
     private router: Router,
-<<<<<<< HEAD
-=======
     private alertService: AlertService,
->>>>>>> 7a357d1b24b87b1a503e3066fbe2151f50aee014
     private fb: FormBuilder
   ) {
     this.formConnect = this.connectForm();
@@ -73,13 +45,10 @@ export class FormulaireConnexionComponent implements OnInit {
       }
     )
   }
-<<<<<<< HEAD
-=======
 
 
 
 
->>>>>>> 7a357d1b24b87b1a503e3066fbe2151f50aee014
 
   submitFormulaireConnexion() {
     this.loginInfo = this.formConnect.value;
@@ -93,10 +62,6 @@ export class FormulaireConnexionComponent implements OnInit {
         this.isLoginFailed = false;
 
         this.roles = this.tokenStorage.getAuthorities();
-<<<<<<< HEAD
-        window.location.reload();
-        this.router.navigate(['../partenaires']);
-=======
 
         if (this.roles[0] == "ROLE_STAGIAIRE") {  this.router.navigate(['../boardstagiaire/gestionstagiaire']); }
         if (this.roles[0] == "ROLE_ENTREPRISE") { this.router.navigate(['../boardentreprise']); }
@@ -104,7 +69,6 @@ export class FormulaireConnexionComponent implements OnInit {
           window.location.reload();
         }, 1);
         
->>>>>>> 7a357d1b24b87b1a503e3066fbe2151f50aee014
       },
       error => {
         console.log(error);
