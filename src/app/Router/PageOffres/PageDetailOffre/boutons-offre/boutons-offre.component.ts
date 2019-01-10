d8d2e5bc0ee448614192d2b6662bdda1d959f4d7
Ioannis Nikas <ioa.nikas@gmail.com>
2019-01-10 17:54:13 +0100

    Mise en page des différents tableau d'offre

--- src/app/Router/PageOffres/PageDetailOffre/boutons-offre/boutons-offre.component.ts
+++ src/app/Router/PageOffres/PageDetailOffre/boutons-offre/boutons-offre.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Offre } from 'src/app/modeles/offre';
 import { Location } from '@angular/common';
+import { TokenStorageService } from 'src/app/auth/token-storage.service';
 
 @Component({
   selector: 'app-boutons-offre',
@@ -10,14 +11,17 @@ import { Location } from '@angular/common';
 export class BoutonsOffreComponent implements OnInit {
 
   @Input() offre: Offre;
+  private role: any;
 
-  constructor(private _location: Location) { }
+  constructor(private _location: Location,
+              private token: TokenStorageService) { }
 
   retourPage() {
     this._location.back();
   }
 
   ngOnInit() {
+    this.role = this.token.getAuthorities();
   }
 
 }
