8552c8e3387151daa3675aa4fcb78cae3ecc27d9
Ioannis Nikas <ioa.nikas@gmail.com>
2019-01-11 10:34:39 +0100

    ajout du nom d'utilisateur connecté dans bord et accueil

--- src/app/Router/BoardStagiaire/page-board-stagiaire/page-board-stagiaire.component.ts
+++ src/app/Router/BoardStagiaire/page-board-stagiaire/page-board-stagiaire.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/app/auth/token-storage.service';
+import { StagiaireService } from 'src/app/services/stagiaire.service';
 
 @Component({
   selector: 'app-page-board-stagiaire',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageBoardStagiaireComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  stagiaire: Object;
+
+  constructor(private token: TokenStorageService,
+    private stagiaireService: StagiaireService) { }
 
   ngOnInit() {
+    this.username = this.token.getUsername();
+    this.stagiaireService.getStagiaire(this.username)
+      .subscribe(data => {
+        this.stagiaire = data;
+      })
   }
 
 }
