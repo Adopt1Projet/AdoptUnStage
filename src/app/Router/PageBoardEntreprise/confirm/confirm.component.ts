55ccbf6bea20a62a4514c5c48677a1293af82752
Kevin Radlowski <kevin.radlowski@gmail.com>
2019-01-15 19:55:14 +0100

    button accueil liste offre + replace all annonce to offre

--- src/app/Router/PageBoardEntreprise/confirm/confirm.component.ts
+++ src/app/Router/PageBoardEntreprise/confirm/confirm.component.ts
@@ -25,7 +25,7 @@ export class ConfirmComponent extends SimpleModalComponent<ConfirmModel, boolean
     // ten we can get dialog result from caller code
     this.result = true;
     this.close();
-    this.alertService.success('Votre annonce a bien été supprimée.', true);
+    this.alertService.success('Votre offre a bien été supprimée.', true);
   }
   cancel() {
     this.result = false;
