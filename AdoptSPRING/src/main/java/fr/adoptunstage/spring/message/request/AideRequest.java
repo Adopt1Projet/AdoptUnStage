2bcec80fe407b539003d0bfdd256deccbbae63c8
Ioannis <ioa.nikas@gmail.com>
2019-01-25 13:22:00 +0100

    fix mineur sur AideRequest / AideService

--- AdoptSPRING/src/main/java/fr/adoptunstage/spring/message/request/AideRequest.java
+++ AdoptSPRING/src/main/java/fr/adoptunstage/spring/message/request/AideRequest.java
@@ -2,21 +2,43 @@ package fr.adoptunstage.spring.message.request;
 
 public class AideRequest {
 
-    private String titre;
+	private String titre;
 
-    private String intertitre;
+	private String intertitre;
 
-    private String texte;
+	private String texte;
 
-    public String getTitre() { return titre; }
+	private String lien;
 
-    public void setTitre(String titre) { this.titre = titre; }
+	public String getLien() {
+		return lien;
+	}
 
-    public String getIntertitre() { return intertitre; }
+	public void setLien(String lien) {
+		this.lien = lien;
+	}
 
-    public void setIntertitre(String intertitre) { this.intertitre = intertitre; }
+	public String getTitre() {
+		return titre;
+	}
 
-    public String getTexte() { return texte; }
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
 
-    public void setTexte(String texte) { this.texte = texte; }
+	public String getIntertitre() {
+		return intertitre;
+	}
+
+	public void setIntertitre(String intertitre) {
+		this.intertitre = intertitre;
+	}
+
+	public String getTexte() {
+		return texte;
+	}
+
+	public void setTexte(String texte) {
+		this.texte = texte;
+	}
 }
